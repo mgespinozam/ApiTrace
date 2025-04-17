e012fcdfe4134fef622dc60e26ffca3b2f3edac1
mgespinozam <mgespinozam@gmail.com>
2025-04-17 17:31:55 -0400

    Add files via upload

---
+++ AssistControlShared/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistControlShared.Models
+{
+    public class Message
+    {
+         public string? mensaj { get; set; }
+        public string? codigo { get; set; }
+        public string? glosa { get; set; }
+
+    }
+
+}
---
+++ AssistControlShared/Models/Trace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistControlShared.Models
+{
+    public class Trace
+    {
+        public int IdUser { get; set; }
+        public int IdActivity { get; set; }
+        public int IdLevel { get; set; }
+        public string? Message { get; set; }
+
+    }
+
+}
